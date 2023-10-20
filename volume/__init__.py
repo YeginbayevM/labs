a57a5c464f29c8cd56a42819b57af0ff77db29dd
YeginbayevM <yeginbayev.psu@gmail.com>
2023-10-20 19:09:27 +0600

    add filter and volume folders

--- volume/__init__.py
+++ volume/__init__.py
@@ -5,32 +5,32 @@ HASHES = ["268f2deee976fcc8dcc915be63cd3d4ac003a73a4b8bfd6f7b95c441a42ed1ec", "4
 
 @check50.check()
 def exists():
-    """volume.c exists"""
+    """volume.c существует"""
     check50.exists("volume.c")
     check50.include("input.wav")
 
 @check50.check(exists)
 def compiles():
-    """volume.c compiles"""
+    """volume.c компилируется"""
     check50.c.compile("volume.c", lcs50=True)
 
 @check50.check(compiles)
 def audio_half():
-    """reduces audio volume, factor of 0.5 correctly"""
+    """громкость звука корректно уменьшается в 0.5 раза"""
     check50.run("./volume input.wav output.wav 0.5").exit(0)
     if check50.hash("output.wav") != HASHES[0]:
         raise check50.Failure("audio is not correctly altered, factor of 0.5")
 
 @check50.check(compiles)
 def audio_tenth():
-    """reduces audio volume, factor of 0.1 correctly"""
+    """громкость звука корректно уменьшается в 0.1 раза"""
     check50.run("./volume input.wav output.wav 0.1").exit(0)
     if check50.hash("output.wav") != HASHES[1]:
         raise check50.Failure("audio is not correctly altered, factor of 0.1")
 
 @check50.check(compiles)
 def audio_x2():
-    """increases audio volume, factor of 2 correctly"""
+    """громкость звука корректно увеличивается в 2 раза"""
     check50.run("./volume input.wav output.wav 2").exit(0)
     if check50.hash("output.wav") != HASHES[2]:
         raise check50.Failure("audio is not correctly altered, factor of 2")
--- volume/volume.c
+++ volume/volume.c
@@ -1,27 +1,42 @@
-#include <cs50.h>
+// Программа на изменение громкости аудиофайла
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, string argv[])
+// Количество байт в заголовке файла .wav
+const int HEADER_SIZE = 44;
+
+int main(int argc, char *argv[])
 {
-    // Check command-line arguments
+    // Проверить наличие аргументов командной строки
     if (argc != 4)
     {
-        printf("Usage: ./music input.wav output.wav factor");
+        printf("Usage: ./volume input.wav output.wav factor\n");
         return 1;
     }
 
-    // Open files and determine scaling factor
+    // Открыть файл и определить коэффициент масштабирования
     FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
     FILE *output = fopen(argv[2], "w");
+    if (output == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
     float factor = atof(argv[3]);
 
-    // TODO: Copy header from input file to output file
+    // TODO: Скопировать заголовок входного файла в выходной файл
 
-    // TODO: Read data from input file and write updated data to output file
+    // TODO: Считать данные сэмпла из входного файла и записать обновлённые данные в выходной файл
 
-    // Close files
+    // Закрыть файл
     fclose(input);
     fclose(output);
 }
