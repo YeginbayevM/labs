9de9b5b996bfbc97c924a3f77c50ae9cc1513b1d
YeginbayevM <yeginbayev.psu@gmail.com>
2024-09-10 09:08:36 +0500

    Update savings.c

--- savings/savings.c
+++ savings/savings.c
@@ -3,50 +3,30 @@
 
 int main(void)
 {
-    // Запрашиваем начальный баланс и проверяем его корректность
-    int start_balance;
-    do
-    {
-        start_balance = get_int("Начальный баланс:")
-    } 
-    while (start_balance <= 0); 
+    // TODO: Запросить начальный баланс и проверить его корректность (start_balance)
 
-    // Запрашиваем процентную ставку и проверяем ее корректность
-    float interest_rate;
-    do
-    {
-        interest_rate = get_float("Процентная ставка:")
-    } 
-    while (interest_rate <= 0);
 
-    // Запрашиваем ежегодное пополнение и проверяем его корректность
-    int yearly_deposit;
-    do
-    {
-       yearly_depost = get_double("Ежегодное пополнение:")
-    } 
-    while (yearly_deposit <= 0); 
+    // TODO: Запросить процентную ставку и проверить её корректность (interest_rate)
+   
 
-    // Запрашиваем целевой баланс и проверяем его корректность
-    int target_balance;
-    do
-    {
-        target_balance = get_double("Целевой баланс:")
-    } 
-    while (target_balance <= start_balance);
+    // TODO: Запросить ежегодное пополнение и проверить его корректность (yearly_deposit)
+  
 
-    // Вычисляем количество лет, необходимых для достижения целевого баланса
+    // TODO: Запросить целевой баланс и проверить его корректность (target_balance)
+
+
+    // TODO: Вычислить количество лет, необходимых для хранения целевого баланса (current_balance)
+    
     int years = 0;
     int current_balance = start_balance;
 
     while (current_balance < target_balance) 
     {
-        // TODO: Реализуйте логику начисления процентов и пополнения счета
+        // TODO: Реализовать логику начисления процентов и пополнения счета
         years++; 
     }
 
-    // Выводим результат
-    printf("Years: %d\n", years);
+    // TODO: Вывести результат
 
     return 0;
 }
