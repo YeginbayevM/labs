b7a39d894ec60f2a27d765fc33725ce2f78b5740
YeginbayevM <yeginbayev.psu@gmail.com>
2024-09-12 02:31:32 +0500

    edit changes

--- mario/hard/__init__.py
+++ mario/hard/__init__.py
@@ -1,4 +1,4 @@
-less = __import__("check50").import_checks("../less")
-from less import *
+normal = __import__("check50").import_checks("../less")
+from normal import *
 
 # mario/more checks are identical to the mario/less checks just with different txt files
