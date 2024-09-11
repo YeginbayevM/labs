b72b644d949d05277d591074af86ef64a112b80b
YeginbayevM <yeginbayev.psu@gmail.com>
2024-09-11 11:51:32 +0500

    init .cs50.yml

---
+++ me/__init__.py
@@ -0,0 +1,58 @@
+import check50
+import check50.c
+import re
+
+
+@check50.check()
+def exists():
+    """hello.c существует"""
+    check50.exists("hello.c")
+
+
+@check50.check(exists)
+def compiles():
+    """hello.c компилируется"""
+    check50.c.compile("hello.c", lcs50=True)
+
+
+@check50.check(compiles)
+def mario():
+    """отвечает на имя Miras"""
+    check_name("Miras")
+
+
+@check50.check(compiles)
+def peach():
+    """responds to name Egghead"""
+    check_name("Egghead")
+
+
+@check50.check(compiles)
+def bowser():
+    """responds to name Lucas"""
+    check_name("Lucas")
+
+
+def check_name(name):
+    # Определяем ожидаемый и фактический вывод программы
+    expected = f"hello, {name}\n"
+    actual = check50.run("./hello").stdin(name).stdout()
+
+    # Проверяем вывод
+    if not re.match(regex(name), actual):
+        try:
+            last_character = actual[-1]
+        except IndexError:
+            raise check50.Mismatch(expected=expected, actual=actual)
+
+        if last_character != "\n":
+            raise check50.Mismatch(
+                expected=expected,
+                actual=actual,
+                help=r"Forgot to print a newline at the end of your output?",
+            )
+        raise check50.Mismatch(expected=expected, actual=actual)
+
+
+def regex(string):
+    return f"^[Hh]ello, {re.escape(string)}\n$"
