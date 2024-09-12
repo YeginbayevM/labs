ded2ea708eb195fb9be5a1d8a520883a94710b74
YeginbayevM <yeginbayev.psu@gmail.com>
2024-09-12 08:30:54 +0500

    translate __init__.py

--- prime/__init__.py
+++ prime/__init__.py
@@ -4,29 +4,29 @@ import check50.c
 
 @check50.check()
 def exists():
-    """prime.c exists"""
+    """prime.c существует"""
     check50.exists("prime.c")
 
 
 @check50.check(exists)
 def compiles():
-    """prime.c compiles"""
+    """prime.c компилируется"""
     check50.c.compile("prime.c", lcs50=True)
 
 
 @check50.check(compiles)
 def up_to_10():
-    """Input of 1 and 10 yields all prime numbers between 1 and 10, inclusive"""
+    """Ввод 1 и 10 выдаёт все простые числа между 1 и 10, включительно"""
     check50.run("./prime").stdin("1").stdin("10").stdout("2\n3\n5\n7")
 
 
 @check50.check(compiles)
 def between_10_and_50():
-    """Input of 10 and 25 yields all prime numbers between 10 and 25, inclusive"""
+    """Ввод 10 и 25 выдаёт все простые числа между 10 и 25, включительно"""
     check50.run("./prime").stdin("10").stdin("25").stdout("11\n13\n17\n19\n23")
 
 
 @check50.check(compiles)
 def between_50_and_60():
-    """Input of 50 and 60 yields all prime numbers between 50 and 60, inclusive"""
+    """Ввод 50 и 60 выдаёт все простые числа между 50 и 60, включительно"""
     check50.run("./prime").stdin("50").stdin("60").stdout("53\n59")
