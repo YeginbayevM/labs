28d642a872df005a808a00a9041868db8530384e
YeginbayevM <yeginbayev.psu@gmail.com>
2024-10-07 09:15:59 +0500

    sort check edit

--- sort/__init__.py
+++ sort/__init__.py
@@ -9,21 +9,24 @@ def exists():
 
 @check50.check(exists)
 def answers():
-    """отвечены все вопросы"""
+    """отвечены все вопросы.
+    Данное задание преподаватель проверит вручную!!
+    """
     content = open("answers.txt", "r").read()
     if "TODO" in content:
         raise check50.Failure("Not all questions answered.")
+    
 
-@check50.check(exists)
-def sorts():
-    """корректно распознаётся каждый тип сортировки"""
+# @check50.check(exists)
+# def sorts():
+#     """корректно распознаётся каждый тип сортировки"""
 
-    check50.log("checking that sorts are classified correctly...")
+#     check50.log("checking that sorts are classified correctly...")
 
-    expected = ["sort1 uses:\s*[Bb][Uu][Bb][Bb][Ll][Ee]", "sort2 uses:\s*[Mm][Ee][Rr][Gg][Ee]", "sort3 uses:\s*[Ss][Ee][Ll][Ee][Cc][Tt][Ii][Oo][Nn]"]
-    actual = open("answers.txt", "r").read()
+#     expected = ["sort1 uses:\s*[Bb][Uu][Bb][Bb][Ll][Ee]", "sort2 uses:\s*[Mm][Ee][Rr][Gg][Ee]", "sort3 uses:\s*[Ss][Ee][Ll][Ee][Cc][Tt][Ii][Oo][Nn]"]
+#     actual = open("answers.txt", "r").read()
 
-    for e in expected:
-        if not search(e, actual):
-            raise check50.Failure("Incorrect assignment of sorts.")
+#     for e in expected:
+#         if not search(e, actual):
+#             raise check50.Failure("Incorrect assignment of sorts.")
 
